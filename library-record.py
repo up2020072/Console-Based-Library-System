0e6cb3e8f8b754142975dc1aadf304a54230b74d
up2020072 <George>
2024-09-20 15:57:39 +0100

    added command line interface

--- library-record.py
+++ library-record.py
@@ -14,18 +14,17 @@ class Book():
         self.num_total = num_total
         self.num_avaialble = num_total
         self.publication_date = publication_date
-        booklist.add_book(self)
         
     def set_title(self, title):
         """Set the title of the book."""
         if not title:
-            raise ValueError("Title cannot be empty.")
+            raise ValueError("\nTitle cannot be empty.")
         self.title = title
 
     def set_author(self, author):
         """Set the author of the book."""
         if not author:
-            raise ValueError("Author cannot be empty.")
+            raise ValueError("\nAuthor cannot be empty.")
         self.author = author
 
     def set_year(self, year):
@@ -33,27 +32,34 @@ class Book():
         if isinstance(year, int) and year > 0:
             self.year = year
         else:
-            raise ValueError("Year must be a positive integer.")
+            raise ValueError("\nYear must be a positive integer.")
 
     def set_publisher(self, publisher):
         """Set the publisher of the book."""
         if not publisher:
-            raise ValueError("Publisher cannot be empty.")
+            raise ValueError("\nPublisher cannot be empty.")
         self.publisher = publisher
 
     def set_num_available(self, num_available):
         """Set the number of available copies."""
-        if isinstance(num_available, int) and num_available >= 0: #this should be reworked
+        if num_available.isnumeric() and int(num_available) >= 0: #this should be reworked
             self.num_avaialble = num_available
         else:
-            raise ValueError("Number of copies must be a non-negative integer.")
+            raise ValueError("\nNumber of copies must be a non-negative integer.")
+    
+    def set_num_copies(self, num_total):
+        """Set the number of total copies."""
+        if num_total.isnumeric() and int(num_total) >= 0: #this should be reworked
+            self.num_total = num_total
+        else:
+            raise ValueError("\nNumber of copies must be a non-negative integer.")
 
     def set_publication_date(self, publication_date):
         """Set the publication date of the book."""
         if isinstance(publication_date, str):
             self.publication_date = publication_date
         else:
-            raise ValueError("Publication date must be a string.")
+            raise ValueError("\nPublication date must be a string.")
         
     def get_title(self):
         """Return the title of the book."""
@@ -75,7 +81,7 @@ class Book():
         """Return the total number of copies of the book."""
         return self.num_copies
 
-    def get_available_copies(self):
+    def get_num_available(self):
         """Return the number of available copies of the book."""
         return self.available_copies
 
@@ -118,6 +124,10 @@ class BookList():
         """Return the total number of books stored in the collection."""
         return len(self.books)
     
+    def __iter__(self):
+        """Allow iteration over the books in the BookList."""
+        return iter(self.books.items())
+    
 class User():
     def __init__(self, username, firstname, lastname, house_num, street_name, postcode, email, dob):
         self.username = username
@@ -128,46 +138,46 @@ class User():
         self.postcode = postcode
         self.email = email
         self.dob = dob #date of birth
-        userlist.add_user(self)
 
     def set_username(self, username):
         if not username:
-            raise ValueError("Username cannot be empty")
+            raise ValueError("\nUsername cannot be empty")
         self.username = username
 
     def set_firstname(self, firstname):
         if not firstname:
-            raise ValueError("Firstname cannot be empty")
+            raise ValueError("\nFirstname cannot be empty")
         self.firstname = firstname
 
     def set_lastname(self, lastname):
         if not lastname:
-            raise ValueError("Lastname cannot be empty")
+            raise ValueError("\nLastname cannot be empty")
         self.lastname = lastname
 
     def set_house_num(self, house_num):
-        if not house_num or not isinstance(house_num, int):
-            raise ValueError("House number must be a non negative integer")
-        self.house_num = house_num
+        if house_num.isnumeric() and int(house_num) >= 0:
+            self.house_num = house_num
+        else:
+            raise ValueError("\nHouse number must be a non negative integer")
 
     def set_street_name(self, street_name):
         if not street_name:
-            raise ValueError("Street name cannot be empty")
+            raise ValueError("\nStreet name cannot be empty")
         self.street_name = street_name
 
     def set_postcode(self, postcode):
         if not postcode:
-            raise ValueError("Postcode cannot be empty")
+            raise ValueError("\nPostcode cannot be empty")
         self.postcode = postcode
 
     def set_email(self, email):
         if not email:
-            raise ValueError("Email cannot be empty")
+            raise ValueError("\nEmail cannot be empty")
         self.email = email
 
     def set_lastname(self, dob):
         if not dob:
-            raise ValueError("Date of birth cannot be empty")
+            raise ValueError("\nDate of birth cannot be empty")
         self.dob = dob
 
     def get_username(self):
@@ -244,6 +254,10 @@ class UserList():
     def get_user(self, username):
         """Return user details by username."""
         return self.users.get(username, None)
+    
+    def __iter__(self):
+        """Allow iteration over the users in the UserList."""
+        return iter(self.users.items())
 
 class Loans():
     def __init__(self):
@@ -278,25 +292,180 @@ class Loans():
     
 
 
-booklist = BookList()
-book1 = Book("The Hobbit", "John Doe", 1960, "Books Inc", 12, "1960-02-24")
-book2 = Book("James Bond", "John Doe", 1930, "Blob Corp", 5, "1930-01-12")
-book3 = Book("1984", "Max Smith", 1930, "Big Publishing", 5, "2008-09-07")
+# print(f'{booklist.get_total_books()} total books')
+# print(f'{userlist.get_total_users()} total users')
 
-userlist = UserList()
-user1 = User("raringgarlic118", "Tim", "Lee", 12, "Park avenue", "BO21HK", "timlee@email.com","2000-01-01")
-user2 = User("blobfish772", "Tim", "Lee", 1, "river avenue", "OJ6XKG", "timlee2@email.com","2002-11-31")
-user3 = User("firequake122", "Bob", "Smith", 1, "birch street", "YTH87P", "bobsmith122@email.com","1992-06-19")
+# # searchtest = booklist.find_book("James Bond")
+# # for b in searchtest:
+# #     print(b)
 
-bookloans = Loans()
+# #userlist.remove_user("Tim")
+# bookloans.borrow_book(user2.username, book1)
+# print(f'{user2.username} is currently borrowing {bookloans.count_borrowed_books(user2.username)} books')
 
-print(f'{booklist.get_total_books()} total books')
-print(f'{userlist.get_total_users()} total users')
+def modify_book(book_list):
+    """Modify a book's details."""
+    try:
+        print("\nBook List:")
+        for book_id, book in list(book_list): print(book)
+        book_id = int(input("\nEnter the Book ID of the book you want to modify:\n"))
+        book = book_list.books.get(book_id)
 
-# searchtest = booklist.find_book("James Bond")
-# for b in searchtest:
-#     print(b)
+        if not book:
+            print(f"\nNo book found with ID: {book_id}\n")
+            main()
+    except ValueError:
+            print("\nInvalid input. Please enter a valid ID.")
+            modify_book(book_list)
 
-#userlist.remove_user("Tim")
-bookloans.borrow_book(user2.username, book1)
-print(f'{user2.username} is currently borrowing {bookloans.count_borrowed_books(user2.username)} books')
+    print(f'\nCurrent details:\n{book}')
+
+    while True:
+        print("\nWhat would you like to modify?")
+        print("1. Title")
+        print("2. Author")
+        print("3. Year")
+        print("4. Publisher")
+        print("5. Number of Copies")
+        print("6. Exit")
+
+        try:
+            choice = input("\nEnter your choice (1-6):\n")
+
+            if choice == '1':
+                new_title = input("\nEnter new title:\n")
+                book.set_title(new_title)
+                break
+            elif choice == '2':
+                new_author = input("\nEnter new author:\n")
+                book.set_author(new_author)
+                break
+            elif choice == '3':
+                new_year = (input("\nEnter new year:\n"))
+                book.set_year(new_year)
+                break
+            elif choice == '4':
+                new_publisher = input("\nEnter new publisher:\n")
+                book.set_publisher(new_publisher)
+                break
+            elif choice == '5':
+                new_copies = (input("\nEnter new number of copies:\n"))
+                book.set_num_copies(new_copies)
+                break
+            elif choice == '6':
+                main()
+            else:
+                print("Invalid choice. Please select again.\n")
+        except ValueError as e:
+            print(e)
+
+    print("\nBook details updated successfully.\n")
+    print(f'New details:\n{book}')
+    main()
+
+def modify_user(user_list):
+    """Modify a user's details."""
+    try:
+        print("\nUser List:")
+        for user_id, user in list(user_list): print(user)
+        username = input("\nEnter the username of the user you want to modify:\n")
+        user = user_list.users.get(username)
+
+        if not user:
+            print(f"\nNo user found with username: {username}\n")
+            main()
+
+    except ValueError:
+            print("\nInvalid input. Please enter a valid username.")
+            modify_book(user_list)
+
+    print(f'\nCurrent details:\n{user}')
+
+    while True:
+        print("\nWhat would you like to modify?")
+        print("1. First Name")
+        print("2. Surname")
+        print("3. House Number")
+        print("4. Street Name")
+        print("5. Postcode")
+        print("6. Exit")
+
+        try:
+            choice = input("\nEnter your choice (1-6): \n")
+
+            if choice == '1':
+                new_firstname = input("\nEnter new first name:\n")
+                user.set_firstname(new_firstname)
+                break
+            elif choice == '2':
+                new_surname = input("\nEnter new surname:\n")
+                user.set_lastname(new_surname)
+                break
+            elif choice == '3':
+                new_house_number = input("\nEnter new house number:\n")
+                user.set_house_num(new_house_number)
+                break
+            elif choice == '4':
+                new_street_name = input("\nEnter new street name:\n")
+                user.set_street_name(new_street_name)
+                break
+            elif choice == '5':
+                new_postcode = input("\nEnter new postcode:\n")
+                user.set_postcode(new_postcode)
+                break
+            elif choice == '6':
+                main()
+            else:
+                print("Invalid choice. Please select again.\n")
+        except ValueError as e:
+            print(e)
+
+    print("\nUser details updated successfully.\n")
+    print(f'New details:\n{user}')
+    main()
+
+def init_library():
+
+    #this could be done with a json file
+
+    book_list = BookList()
+    user_list = UserList()
+    book_loans = Loans()
+
+    book_list.add_book(Book("The Hobbit", "John Doe", 1960, "Books Inc", 12, "1960-02-24"))
+    book_list.add_book(Book("James Bond", "John Doe", 1930, "Blob Corp", 5, "1930-01-12"))
+    book_list.add_book(Book("1984", "Max Smith", 1930, "Big Publishing", 5, "2008-09-07"))
+
+    user_list.add_user(User("raringgarlic118", "Tim", "Lee", 12, "Park avenue", "BO21HK", "timlee@email.com","2000-01-01"))
+    user_list.add_user(User("blobfish772", "Tim", "Lee", 1, "river avenue", "OJ6XKG", "timlee2@email.com","2002-11-31"))
+    user_list.add_user(User("firequake122", "Bob", "Smith", 1, "birch street", "YTH87P", "bobsmith122@email.com","1992-06-19"))
+
+    # print(f'{book_list.get_total_books()} total books')
+    # print(f'{user_list.get_total_users()} total users')
+
+    return book_list, user_list, book_loans
+
+def main():
+    book_list, user_list, book_loans, = init_library()
+    print("\nWelcome to the Library System!")
+    while True:
+        print(f'1. Modify a Book\n'
+              f'2. Modify a User\n'
+              f'3. Exit\n')
+        try:
+            choice = int(input("Please specify which option you would like (enter the number): \n"))
+            if choice == 1:
+                modify_book(book_list)
+                break
+            elif choice == 2:
+                modify_user(user_list)
+                break
+            elif choice == 3:
+                break
+            else:
+                print("\nInvalid input, please pick one of the following options.")
+        except ValueError:
+                print("\nInvalid input. Please enter a valid number.")
+
+if __name__ == '__main__':
+    main()
